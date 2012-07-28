b6ef19517c63443e335d76e58e8a83c3e3d9531a
Scott Shepherd <ScottMShepherd@gmail.com>
2012-07-27 23:37:44 -0500

    Base form for the GUI. A template, essentially.

---
+++ gui_queens.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+import queens
+import search_queens 
+
+class QueenDisplay(QDialog):
+    """
+    Main form for displaying the 8 queens chess board along with various
+    ways to interact with the chessboard and change its appearance and such.
+    """
+
+    def __init__(self, parent=None):
+        super(QueenDisplay, self).__init__(parent)
+        self.setWindowTitle("8 Queens Problem")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    display = QueenDisplay()
+    display.show()
+
+    sys.exit(app.exec_())
