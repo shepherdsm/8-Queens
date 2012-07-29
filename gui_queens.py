f267243271c1d8fc1cb47b24f178d8e83260a2f8
Scott Shepherd <ScottMShepherd@gmail.com>
2012-07-28 22:34:20 -0500

    Adding in comments. Removed an extra variable that didn't need to exist. Starting on the control widget

--- gui_queens.py
+++ gui_queens.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+"""
+My GUI for displaying the chess board solutions of the 8-queens problem.
+This is my first time using Qt for drawing a GUI, and I must say it's quite
+fun. Last time I needed a GUI in Python was with Tkinter, and it can't compare
+at all so far. Definitely a nice improvement.
+
+Author: Scott Shepherd
+Version: 1.0
+"""
+
 import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
@@ -9,10 +19,17 @@ import queens
 import search_queens 
 
 class ChessBoardWidget(QWidget):
-    def __init__(self, size=8):
+    """
+    Creates a chessboard widget that gets drawn onto the screen and updated
+    when certain controls are changed.
+
+    TODO: Function that draws queens.
+    """
+    def __init__(self, size=8, sides=10):
         super(ChessBoardWidget, self).__init__()
 
         self.board_size = size
+        self.square_sides = sides
 
         self.square_color1 = (0, 255, 0)
         self.square_color2 = (0, 0, 0)
@@ -35,19 +52,21 @@ class ChessBoardWidget(QWidget):
         qp.end()
 
     def draw_squares(self, qp, first_square, color):
-        sides = 20
-
+        """
+        Draws the squares onto the widget. The variable first_square doesn't
+        particularly matter as long as it's an int and they're a different parity.
+        """
         offset_w = offset_h = 0        
 
         qp.setPen(QColor(*color))
         qp.setBrush(QColor(*color))
 
         for i in range(first_square, self.board_size + first_square):
-            offset_w = sides * (i % 2)
+            offset_w = self.square_sides * (i % 2)
             for j in range(i % 2, self.board_size, 2):
-                qp.drawRect(offset_w, offset_h, sides, sides)
-                offset_w += sides * 2
-            offset_h += sides
+                qp.drawRect(offset_w, offset_h, self.square_sides, self.square_sides)
+                offset_w += self.square_sides * 2
+            offset_h += self.square_sides
 
     def drawWidget(self, qp):
         # Draw first set of squares
@@ -64,8 +83,7 @@ class QueenDisplay(QMainWindow):
     def __init__(self, parent=None):
         super(QueenDisplay, self).__init__()
 
-        self.board = ChessBoardWidget()
-        #self.board.update()
+        self.board = ChessBoardWidget(10)
 
         self.init_UI()
 
@@ -73,18 +91,36 @@ class QueenDisplay(QMainWindow):
     def init_UI(self):
         self.setCentralWidget(self.main_widget())
         
-        self.setGeometry(300, 800, 250, 180)
+        self.setGeometry(300, 300, 500, 500)
         self.setWindowTitle("8 Queens Problem")
         self.show()
 
     def main_widget(self):
+        """
+        Since Windows can't have a display layout really, creating
+        a main widget to act as the central widget for the program.
+        The main widget contains the layout for everything in the program.
+        """
         main_widget = QWidget(self)
 
         main_box = QHBoxLayout()
+        main_box.addWidget(self.control_widget())
         main_box.addWidget(self.board)
         main_widget.setLayout(main_box)
 
         return main_widget
+
+    def control_widget(self):
+        """
+        A widget to define the control structure used to control the display
+        of the chess board widget.
+        """
+        con_wid = QWidget()
+
+        size_label = QLabel("Size")
+        size_edit = QLineEdit()
+
+        grid = QGridLayout()
         
 def main():
     app = QApplication(sys.argv)
